1a1798e234eb8341efbb2e3c5793239b8585b25c
Huwen281127 <125277571+Huwen281127@users.noreply.github.com>
2023-03-20 13:53:52 +0800

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LengthCalculator1
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douCM;
+            douCM = Convert.ToDouble(txtCM.Text);
+         
+            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
+            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
+            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
+            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
+            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+        }
+
+        private void txtM_KeyUp(object sender, KeyEventArgs e)
+        {
+            double douM; 
+            douM = Convert.ToDouble(txtM.Text); 
+
+            txtCM.Text = string.Format("{0:0.##########}", douM / 0.01);
+            txtKM.Text = string.Format("{0:0.##########}", douM / 1000);
+            txtIn.Text = string.Format("{0:0.##########}", douM / 0.0254);
+            txtFt.Text = string.Format("{0:0.##########}", douM / 0.3048);
+            txtYard.Text = string.Format("{0:0.##########}", douM / 0.9144);
+        }
+    }
+}
